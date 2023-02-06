e60ab5f80054c0d1bda2e8d228ebe3e568d3f839
Nitesh kumar <120910923+nitesh7488@users.noreply.github.com>
2023-02-06 19:47:26 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,26 @@
+class vechicles{
+    wheels,brand,mileage,color;
+    constructor(){
+       this.wheel=2;
+       this.brand='bajaj';
+       this.mileage='30kmpl';
+       this.color='black';
+    }
+    constructor( wheel,brand,mileage,color){
+        this.wheel=wheel;
+        this.brand=brand;
+        this.mileage=mileage;
+        this.color=black; 
+
+    }
+    display(){
+        document.write("brand is:"+this.brand+" color is:"+this.color);
+
+    }
+}
+let scooter=new vechicle(2,'bajaj','30kmpl','black');
+scooter.display();
+let maruti=new vechicle(4,'maruti alto','40kmpl','red');
+maruti.display();
+let fname=new String('nitesh')
+console.log(fname);
